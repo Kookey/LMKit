fed00219e17412f5c819bd6ba51edc512440be99
李蒙 <921854304@qq.com>
2015-07-28 14:46:30 +0800

    init

---
+++ LMKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMKitDemo
+//
+//  Created by 李蒙 on 15/7/28.
+//  Copyright (c) 2015年 李蒙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
