60907a2390b65b73fb876da5fa56ad8696379a6e
李蒙 <921854304@qq.com>
2015-08-14 14:41:13 +0800

    LMKit

--- LMKit/LMCategory/LMMacro.h
+++ LMKit/LMCategory/LMMacro.h
@@ -1,6 +1,6 @@
 //
 //  LMMacro.h
-//  LMKitDemo
+//  LMCategory
 //
 //  Created by 李蒙 on 15/8/14.
 //  Copyright (c) 2015年 李蒙. All rights reserved.
--- LMKit/LMKit.h
+++ LMKit/LMKit.h
@@ -1,6 +1,6 @@
 //
 //  LMKit.h
-//  LMKitDemo
+//  LMKit
 //
 //  Created by 李蒙 on 15/7/28.
 //  Copyright (c) 2015年 李蒙. All rights reserved.
